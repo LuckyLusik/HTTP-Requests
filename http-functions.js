81f022701de5a20a4de869068461d5c760217af5
Lucy <lucyneatdesign@yahoo.ca>
2018-12-03 18:55:03 +0000

    function is nested happily in its own little module

---
+++ http-functions.js
@@ -0,0 +1,27 @@
+module.exports = function getHTML (options, callback) {
+
+  var fullData = '';
+  var https = require('https');
+  https.get(options, function (response) {
+
+  // set encoding of received data to UTF-8
+  response.setEncoding('utf8');
+
+  // the callback is invoked when a `data` chunk is received
+  response.on('data', function (data) {
+    fullData += data;
+    //console.log('Chunk Received:', fullData);
+  });
+
+  // the callback is invoked when all of the data has been received
+  // (the `end` of the stream)
+  response.on('end', function() {
+    callback(fullData);
+  });
+
+});
+
+}
+
+
+
